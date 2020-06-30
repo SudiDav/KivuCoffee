5465805b8d99242d595ba32edea6670dfbecd2b4
Sudi Dav <sudisimbadav@gmail.com>
2020-06-30 03:45:09 +0200

    added the dbContext Configurations and the Customer Class

---
+++ KivuCoffee.Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using KivuCoffee.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KivuCoffee.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+    }
+}
---
+++ KivuCoffee.Data/Models/CustomerAddress.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KivuCoffee.Data.Models
+{
+    public class CustomerAddress
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        [MaxLength(100)]
+        public string AddressLine1 { get; set; }
+
+        [MaxLength(100)]
+        public string AddressLine3 { get; set; }
+
+        [MaxLength(100)]
+        public string City { get; set; }
+
+        [MaxLength(10)]
+        public string Street { get; set; }
+
+        [MaxLength(10)]
+        public string Postalcode { get; set; }
+
+        [MaxLength(50)]
+        public string Country { get; set; }
+    }
+}
